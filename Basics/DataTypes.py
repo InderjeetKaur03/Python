c4d4889b47f42d91e81c4d501488c404fa2a310e
InderjeetKaur03 <164217855+InderjeetKaur03@users.noreply.github.com>
2024-05-16 17:00:03 -0400

    Update DataTypes.py
    Updated

--- Basics/DataTypes.py
+++ Basics/DataTypes.py
@@ -1,1 +1,12 @@
+## String operations
+# Numeric
+# Regular expressions - functions
 
+name="Inderjeet Kaur"
+arn ="arn:aws:iam:123456789:user/inder"
+print("Split function" + " "+arn.split("/")[1])
+print("Upper Case : " + name.upper())
+print("Upper Case : " + name.lower())
+print("Length function : ", len(name))
+print("Replace : " + name.replace("jeet","jit"))
+print(name.split())
